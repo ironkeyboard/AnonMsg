77d2eaa683c92365125e34d26b4365d435b964a0
Leo Sekour <fishingfights@gmail.com>
2020-11-13 02:50:40 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const ejs = require('ejs');
+const io = require('socket.io')(http);
+const marked = require('marked');
+const fs = require('fs');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.render("index");
+});
+
+io.sockets.on('connection', (socket) => {
+  var now= new Date();
+  socket.on('username', function(username) {
+    socket.username = username;
+
+    if (username){
+        io.emit('is_online', 'welcome <i>' + socket.username + '!</i>');
+    }
+  });
+
+  socket.on('disconnect', (username) => {
+    if (username){
+      io.emit('is_online', 'goodbye <i>' + socket.username + '!</i>');
+    }
+  });
+
+  socket.on('chat_message', (message) => {
+      if (socket.username == undefined){
+          socket.username = "expired"
+      }
+      var color = 'style = "color: black"'
+      if (socket.username.toLowerCase() == "mckenna"){
+        color = 'style = "color: green"';
+      };
+      if (socket.username.toLowerCase() != ""){
+        io.emit('chat_message',`</strong> [${now.toLocaleDateString("en-us")}]</strong>` +  `<strong ${color}> ` + socket.username + '</strong> ⟶ ' + message);
+        fs.appendFileSync('messages.txt', `[${now.toLocaleDateString("en-us")}] ` +   socket.username + ' ⟶ ' + message + "\n");
+      }
+  });
+});
+
+const server = http.listen(8080, () => {
+  console.log('listening on *:8080');
+});
